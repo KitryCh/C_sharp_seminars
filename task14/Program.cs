58a44a17c6fa2702b727777f725f11e6c4492c30
Ekaterina Cheremnykh <eafilippova@gmail.com>
2022-12-09 17:02:14 +0300

    example13,14,15,16

---
+++ task14/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+int ProductionToOne(int number)
+{
+    var production = 1;
+    if(number < 0)
+    production = 0;
+    else
+    {
+    for(int i = number; i > 1; i--)
+         production *= i;   
+    }
+    return production;
+}
+Console.Write("Введите число: ");
+var num = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Произведение всех цифр: {ProductionToOne(num)}.");
---
+++ task15/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+int[] FillArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(0, 2);
+    return array;
+}
+void PrintArray(int[]array)
+{
+    for(int i = 0; i < array.Length; i++)
+    Console.Write($"{array[i]} - ");
+}
+int[] array = new int[8];
+
+array = FillArray(array);
+PrintArray(array);
